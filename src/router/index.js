b2a30bd24364e48ed43f75f613f86ecd58c78ab6
unknown <you>
2019-12-18 15:34:24 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Home from '../views/home'
+import Login from '../views/login'
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: Home
-  // },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    component: Login
+  }
   // {
   //   path: '/about',
   //   name: 'about',
