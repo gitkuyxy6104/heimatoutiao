23985808fd704354532c86b8c75f102b1363a820
unknown <you>
2019-12-21 19:49:01 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import ceList from './home/ce-list.vue'
 import aheader from './home/header.vue'
+import BreadCrumb from './common/bread-crumb.vue'
 
 export default {
   install (Vue) {
     Vue.component('a-lista', ceList) // 全局注册
     Vue.component('a-header', aheader) // 全局注册
+    Vue.component('bread-crumb', BreadCrumb)
   }
 }
