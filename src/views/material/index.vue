487377e35eb0d946f95d0d87849ffa47092f2128
unknown <you>
2019-12-22 21:00:30 +0800

    黑马头条PC-素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,13 +1,83 @@
 <template>
-  <div>aaaa</div>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-tabs @tab-click="handleClick" v-model="activeName">
+      <el-tab-pane label="全部素材" name="one">
+        <!-- 全部 -->
+       <!-- 全部素材内容 定制 -->
+              <div class="img-list">
+                <!-- v-for -->
+                 <el-card class='img-card' v-for="item in list" :key="item.id">
+                     <img :src="item.url" alt="">
+                    <el-row class='operate' type='flex' align="middle" justify="space-around" >
+                        <i class='el-icon-star-on'></i>
+                        <i class='el-icon-delete-solid'></i>
+                    </el-row>
+                 </el-card>
+              </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="two">
+        <!-- 收藏 -->
+        <div class="img-list">
+                <!-- v-for -->
+                 <el-card class='img-card' v-for="item in list" :key="item.id">
+                     <img :src="item.url" alt="">
+                 </el-card>
+              </div>
+      </el-tab-pane>
+    </el-tabs>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'one', // 默认选中全部
+      list: [] // 接收全部数据
+    }
+  },
+  methods: {
+    handleClick () {},
+    getAllMaterial () {
+      this.$axios({
+        url: '/user/images',
+        paras: { collect: this.activeName === 'collect' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getAllMaterial()
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+ .img-list {
+     display: flex;
+     flex-wrap: wrap;
+     .img-card {
+         width: 200px;
+         height:240px;
+         margin: 20px 50px;
+         position: relative;
+         img {
+             width: 100%;
+             height: 100%;
+         }
+         .operate {
+             width: 100%;
+             font-size:20px;
+             position: absolute;
+             bottom: 0;
+             left:0;
+             background-color: #f4f5f6;
+             height:30px;
+         }
+     }
+ }
 </style>
