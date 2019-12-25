5b8da1966068c03846215334a5a0d9d89e2c2715
unknown <you>
2019-12-25 16:12:27 +0800

    -发布文章-封面功能设计

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,16 @@
 import ceList from './home/ce-list.vue'
 import aheader from './home/header.vue'
 import BreadCrumb from './common/bread-crumb.vue'
+import { quillEditor } from 'vue-quill-editor' // 引入了quill组件
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 export default {
   install (Vue) {
     Vue.component('a-lista', ceList) // 全局注册
     Vue.component('a-header', aheader) // 全局注册
     Vue.component('bread-crumb', BreadCrumb)
+    Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
   }
 }
