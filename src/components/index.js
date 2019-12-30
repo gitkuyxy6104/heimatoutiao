5c4c2891829cbefca030afaf5be277b14ccdaf76
unknown <you>
2019-12-30 16:12:03 +0800

    发布文章-封面图片选择组件封装及使用

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import aheader from './home/header.vue'
 import BreadCrumb from './common/bread-crumb.vue'
 import { quillEditor } from 'vue-quill-editor' // 引入了quill组件
 import CoverImage from './publish/cover-image'
+import SelectImage from './publish/select-image'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
@@ -14,5 +15,6 @@ export default {
     Vue.component('bread-crumb', BreadCrumb)
     Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
     Vue.component('cover-image', CoverImage) // 注册一个封面组件
+    Vue.component('select-image', SelectImage) // 注册一个选择素材的组件
   }
 }
