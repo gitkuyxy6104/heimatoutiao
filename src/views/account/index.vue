0d90040e03f4321ec48acbb51a1a09141f0e086a
unknown <you>
2019-12-30 17:29:24 +0800

    账户信息更新-同步到头部页面之evenBus

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -29,6 +29,7 @@
 </template>
 
 <script>
+import eventBus from '../../gongju/eventBus' // 引入了公共的实例
 export default {
   data () {
     return {
@@ -68,6 +69,7 @@ export default {
         data
       }).then(result => {
         this.formData.photo = result.data.photo // 设置头像地址
+        eventBus.$emit('updateUserInfoSuccess') // 触发一个自定义事件 updateUserInfoSuccess
         this.loading = false // 关调弹层
       })
     },
@@ -96,6 +98,9 @@ export default {
               type: 'success',
               message: '保存信息成功'
             })
+            // 需要告诉头部组件 我更新成功了 你也要更新
+            // this.$emit('')
+            eventBus.$emit('updateUserInfoSuccess') // 触发一个自定义事件 updateUserInfoSuccess
           })
         }
       })
